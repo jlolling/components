e8f312a2ad0d2c19490a4eafe49dbc3cc4cbdb77
hmdebenque <hmdebenque@talend.com>
2016-11-10 17:52:52 +0100

    TCOMP-262: start implementations of Properties controller

--- services/components-api-service-rest/src/main/java/org/talend/components/service/rest/PropertiesController.java
+++ services/components-api-service-rest/src/main/java/org/talend/components/service/rest/PropertiesController.java
@@ -11,8 +11,11 @@
 
 package org.talend.components.service.rest;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.talend.daikon.annotation.Service;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
@@ -20,6 +23,7 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @Service(name = "PropertiesController")
+@RestController
 @RequestMapping(value = "properties",
                 consumes = APPLICATION_JSON_UTF8_VALUE,
                 produces = APPLICATION_JSON_UTF8_VALUE)
@@ -27,12 +31,13 @@ public interface PropertiesController {
 
     @RequestMapping(value = "{name}", method = GET)
         // P1
-    void getProperties(@PathVariable("name") String definitionName);
+    String getProperties(@PathVariable("name") String definitionName);
 
     /** Validate **/
     // P1
     @RequestMapping(value = "{definitionName}/validate", method = POST)
-    void validateProperties(@PathVariable("definitionName") String definitionName);
+    ResponseEntity<PropertiesValidationResponse> validateProperties(@PathVariable("definitionName") String definitionName,
+                                                                    @RequestBody FormDataContainer formData);
 
     /** Validate one field. */
     // P1
@@ -54,10 +59,9 @@ public interface PropertiesController {
     @RequestMapping(value = "{definitionName}/beforeActivate/{propName}", method = POST)
     void beforeActivateProperty(@PathVariable("definitionName") String definitionName, @PathVariable("propName") String propName);
 
-    // P1
-
     /** Get dataset properties. Should it be GET? **/
+    // P1
     @RequestMapping(value = "{definitionName}/dataset", method = POST)
-    void getDataset();
+    String getDatasetProperties(@PathVariable("definitionName") String definitionName, @RequestBody FormDataContainer formData);
 
 }
